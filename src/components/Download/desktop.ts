1e9dafad0120bfcc6b1e18c129014a27ddf6e7a9
Zach Kelling <z@zeekay.io>
2024-09-16 10:22:04 -0700

    Update download links

--- src/components/Download/desktop.ts
+++ src/components/Download/desktop.ts
@@ -1,8 +1,8 @@
 export const DESKTOP_DOWNLOAD_URL = {
   macosIntel:
-    'https://download.rabby.io/wallet-desktop/darwin-x64/rabby-wallet-desktop-installer-x64-latest.dmg',
+    'https://dl.luxwallet.xyz/wallet-desktop/darwin-x64/lux-wallet-desktop-installer-x64-latest.dmg',
   macosArm:
-    'https://download.rabby.io/wallet-desktop/darwin-arm64/rabby-wallet-desktop-installer-arm64-latest.dmg',
+    'https://dl.luxwallet.xyz/wallet-desktop/darwin-arm64/lux-wallet-desktop-installer-arm64-latest.dmg',
   windows:
-    'https://download.rabby.io/wallet-desktop/win32-x64/rabby-wallet-desktop-installer-x64-latest.exe'
+    'https://dl.luxwallet.xyz/wallet-desktop/win32-x64/lux-wallet-desktop-installer-x64-latest.exe'
 };
--- src/components/Download/mobile.ts
+++ src/components/Download/mobile.ts
@@ -1,5 +1,5 @@
 export const MOBILE_DOWNLOAD_URL = {
   googlePlay:
-    "https://play.google.com/store/apps/details?id=com.debank.rabbymobile",
-  selfHost: "https://download.rabby.io/downloads/wallet-mobile/android/rabby-mobile.apk"
+    "https://play.google.com/store/apps/details?id=network.lux.wallet",
+  selfHost: "https://dl.luxwallet.xyz/mobile/android/lux-android.apk"
 };
